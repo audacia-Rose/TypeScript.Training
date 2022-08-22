5624787fd3c2f0655388c6769dd45dc7479c4042
audacia-Rose <rose.hadley@audacia.co.uk>
2022-08-22 15:03:56 +0100

    package install, corrections
    installed readline-sync packages, corrected some code for date, - still incomplete

--- Social-Media-App/social-media-app.ts
+++ Social-Media-App/social-media-app.ts
@@ -14,7 +14,6 @@ let newFirst = ""
 let newSur = ""
 let newEmail = ""
 let newBirth = ""
-let newUser = new User("", "", "", new Date())
 
 function getUserInput() {
     var answer = question("Please choose a number. ");
@@ -22,46 +21,65 @@ function getUserInput() {
     switch (+answer) {
         case 1:
             console.log("Thank you for choosing to create a new user.");
-             
-            let newFirst = String(question("Please enter your first name: "));
-            let newSur = String(question("Please enter your surname: "));
-            let newEmail = String(question("Please enter your email: "));
-            let newBirth = question("Please enter your date of birth in the format dd/mm/yyyy: ");
-            newBirth = newBirth.split("/");
-            newBirth = Array.from(newBirth, Number);
-            let newUser = new User(newFirst, newSur, newEmail, new Date(newBirth));
-            listOfUsers.push(newUser);
-            question("Please type Yes if this information is correct: " + newUser);
+            let confirmY = ""             
+            do {
+                let newFirst = String(question("Please enter your first name: "));
+                let newSur = String(question("Please enter your surname: "));
+                let newEmail = String(question("Please enter your email: "));
+                let newBirth = question("Please enter your date of birth in the format dd/mm/yyyy: ");
+                let splitNewBirth: string[] = newBirth.split("/");
+                let numberNewBirth: number[] = Array.from(splitNewBirth, Number);
+                numberNewBirth = numberNewBirth.reverse();
+                let newUser = new User(newFirst, newSur, newEmail, new Date(numberNewBirth[0], numberNewBirth[1], numberNewBirth[2]));
+                confirmY = question("Please type Yes if this information is correct: " + newUser);                
+                listOfUsers.push(newUser);
+            } while (confirmY !== "Yes" || "yes");  
             break;
         case 2:
             console.log("You have chosen to update your user account.")
             let updateFirstName = question("Please enter a new first name, or press Enter to skip this section.")
             if (updateFirstName === "") {
+                updateFirstName = newFirst
             }
-            else {newFirst = updateFirstName
-            }
+            else {}
             let updateSurname = question("Please enter a new surname, or press Enter to skip this section.")
             if (updateSurname === "") {
-            } else {newSur = updateSurname
-            }
+                updateSurname = newSur
+            } else {}
             let updateEmail = question("Please enter a new email, or press Enter to skip this section.")
             if (updateEmail === "") {
-            } else {newEmail = updateEmail
+                updateEmail = newEmail
+            } else {
             }            
-            let updateBirthday = question("Please enter a new date of birth, or press Enter to skip this section.")
-            if (updateBirthday === "") {
-            } else {newBirth = updateBirthday
+            let updateBirth = question("Please enter a new date of birth in the form dd/mm/yyyy, or press Enter to skip this section.")
+            if (updateBirth === "") {
+                updateBirth = newBirth 
+            } else {
+                let splitUpdateBirth: string[] = updateBirth.split("/");
+                let numberUpdateBirth: number[] = Array.from(splitUpdateBirth, Number);
+                numberUpdateBirth = numberUpdateBirth.reverse()
             }
-            break;
-            let updatedUser = new User(newFirst, newSur, newEmail, newBirth)
-            listOfUsers.pop()
+            listOfUsers.pop() /*Can I find the index of the user's details in the array and splice instead?*/
+            let updatedUser = new User(newFirst, newSur, newEmail, new Date(numberUpdateBirth[0], numberNewBirth[1], numberNewBirth[2]))
+            listOfUsers.push(updatedUser)
+            break;           
         case 3:
             console.log("You have chosen to delete your user account.")
-            let deleteYN = question("Are you sure you want to delete this user? Please type Yes or No")
-
+            let deleteYN = question("Are you sure you want to delete this user? Please type Yes or No.")
+            if (deleteYN = "Yes" || "yes") {
+                listOfUsers.pop()
+                console.log("You have now deleted this user.")
+            }
+            else if (deleteYN = "No" || "no") {
+                console.log("Okay. We will not delete this user account.")
+                getUserInput()
+            }
+            else {
+                console.log("Invalid input: Please type in Yes or No.")
+            }
             break;
         default:
-            console.log("Please enter the number 1, 2, or 3.")
+            console.log("Invalid input: Please enter the number 1, 2, or 3.")
             getUserInput()
             break;
     }
